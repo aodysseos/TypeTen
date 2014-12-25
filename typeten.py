a2773f0dc141541508e2172dec83be75ee6a9b5a
Soumya Sundarrajan <soumya.rajan86@gmail.com>
2014-12-25 17:03:02 +0000

    Retrieving answers from db
    I've retrieved values from db and populated the the appropriate html
    files. Removed redundant code to avoid confusion. Established connection
    between python and html files.

--- typeten.py
+++ typeten.py
@@ -2,30 +2,90 @@
 import cgi
 import urllib
 import random
+import json
 import jinja2
 import os
 
-
-from models import *
 from google.appengine.api import users
 import webapp2
 from decorators import admin_required
+from google.appengine.ext import ndb
+from random import randint
 
 #set jinja2 environment to connect html with python
 jinja_environment = jinja2.Environment(autoescape=True,
                                        loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),
                                                                                    'views')))
 
+# Sets model for data we're going to store, properties and attributes.
+class Question(ndb.Model):
+    _use_memcache = False
+    content = ndb.StringProperty(required=True, indexed=True)
+    number = ndb.IntegerProperty(required=True, indexed=True)
 
-#3 Establishes initial parent key to group entities
-DEFAULT_TYPETEN_NAME = 'default_typeten'
+class Answer_rating(ndb.Model):
+    _use_memcache = False
+    description = ndb.StringProperty(required=True, indexed=False)
+    points = ndb.IntegerProperty(required=True, indexed=False)
 
+class Answer(ndb.Model):
+    _use_memcache = False
+    content = ndb.StringProperty(required=True, indexed=False)
+    question_id = ndb.IntegerProperty(required=True)
+    rating_id = ndb.IntegerProperty(required=True)
 
-def typeten_key(typeten_name=DEFAULT_TYPETEN_NAME):
-    return ndb.Key('Typeten', typeten_name)
+class GameUser(ndb.Model):
+    username = ndb.StringProperty(required=True)
 
+class Score(ndb.Model):
+    score = ndb.IntegerProperty()
+    time = ndb.IntegerProperty()
+    user_id = ndb.IntegerProperty(required=True)
+#End
 
-#5 Creates MainPage content
+#Initialises the tables
+class InitialiseDatabase(webapp2.RequestHandler):
+    def get(self):
+       question = Question()
+       question.content = "What are the highlights of France?"
+       question.number = 1
+       question_key=question.put()
+
+       answer_rating1 = Answer_rating()
+       answer_rating1.description='low'
+       answer_rating1.points=5
+       answer_rating1_key=answer_rating1.put()
+
+       answer_rating2 = Answer_rating()
+       answer_rating2.description='medium'
+       answer_rating2.points=8
+       answer_rating2_key=answer_rating2.put()
+
+       answer_rating3 = Answer_rating()
+       answer_rating3.description='high'
+       answer_rating3.points=10
+       answer_rating3_key=answer_rating3.put()
+
+       answer1 = Answer()
+       answer1.content = 'Eiffel Tower'
+       answer1.question_id = question.number
+       answer1.rating_id = answer_rating1_key.id()
+       answer1.put()
+
+       answer2 = Answer()
+       answer2.content='Disneyland Paris'
+       answer2.question_id=question.number
+       answer2.rating_id=answer_rating2_key.id()
+       answer2.put()
+
+       answer3 = Answer()
+       answer3.content='Mont Blanc'
+       answer3.question_id=question.number
+       answer3.rating_id=answer_rating3_key.id()
+       answer3.put()
+       self.redirect('/MainPage')
+
+#Creates MainPage content
 class MainPage(webapp2.RequestHandler):
     def get(self):
 
@@ -48,29 +108,19 @@ class MainPage(webapp2.RequestHandler):
         template = jinja_environment.get_template('draft.html')
         self.response.write(template.render(template_values))
 
+class NewGame(webapp2.RequestHandler):
+    def get(self):
 
-#6 Stores data
-class TypetenDataModel(webapp2.RequestHandler):
-    # Uses same ancestor key
-    def post(self):
-        typeten_name = self.request.get('typeten_name',
-                                        DEFAULT_TYPETEN_NAME)
+        # Query to retrieve a particular question using random number generator
+        rand = randint(1,4)
+        question = Question.query(Question.number == rand).get()
 
-        #User and Score table will be created and initialised when we create a login feature using gmail id.
-        currentuser = users.get_current_user()
-        if currentuser:
-            user = GameUser(parent=typeten_key(typeten_name))
-            user.username = currentuser.nickname()
-            user_key = user.put()
-            score = Score(parent=typeten_key(typeten_name))
-            score.score = 0
-            score.time = 0
-            score.user_id = user_key.id()
+        template_values = {
+            'question': question.content
+        }
 
-        # Redirects to app root, with parameters
-        query_params = {'typeten_name': typeten_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
-#6 End
+        template = jinja_environment.get_template('game.html')
+        self.response.write(template.render(template_values))
 
 
 class AdminPage(webapp2.RequestHandler):
@@ -81,6 +131,8 @@ class AdminPage(webapp2.RequestHandler):
 
 
 application = webapp2.WSGIApplication([
-('/', MainPage),
-('/admin', AdminPage)
+('/', InitialiseDatabase),
+('/MainPage', MainPage),
+('/game.html',NewGame),
+('/admin_draft.html',AdminPage)
 ], debug=True)
