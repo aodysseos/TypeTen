c64df81e73081a3910d887e57ae7120f434119c3
fstevenson <fernandosv88@gmail.com>
2014-12-29 02:57:24 +0000

    Changes for admin module
    In the check questions section now it is possible to add more fields for
    more answers (the backend was modified to send the answer rating as
    well).
    
    In the new question the data is passed to the back end: content of the
    question, plus 'n' number of answers (including rating). However, at the
    moment the values are not saved in the db.

--- scripts/admin.js
+++ scripts/admin.js
@@ -110,6 +110,52 @@ function addAnswer(id) {
 }
 
 //
+// Function createAnswer
+// 
+// Function to create a new answer for an existing question.
+function createAnswer(numberId, questionId) {
+    var number = Number(numberId);
+    console.log($('#answer_value_' + number).val());
+    console.log($('#difficulty_answer_' + number).val());
+    $.ajax({
+            type: "POST",
+            //url: // to define
+            dataType: 'json',
+            ContentType: 'application/json',
+            data: {answer_id: realID}
+        })
+            .success(function (data) {
+                alert("ok");
+                //$('#answer_' + realID).parent().remove();
+            })
+            .error(function () {
+                alert("error");
+                
+            })
+        return false;
+}
+
+function addAnswerExistingQuestion(id, questionId) {
+    console.log("ID: " + id);
+    var tmp = id.split('_');
+    var realID = tmp[2];
+    var numberID = Number(realID) + 1;
+    $('#answer_results_' +  Number(realID)).after('<div id="answer_results_' + numberID + '" class="pure-control-group">'
+        + '<label for="answer_' + numberID + '">Answer # ' + numberID + '</label><input id="answer_value_' + numberID + '" class="pure-input-1-3" type="text" placeholder="New answer">' 
+        + '<select id="difficulty_answer_' + numberID + '" style="margin-left: 0.3rem">'
+        + '<option value="low">low</option>'
+        + '<option value="medium">medium</option>'
+        + '<option value="high">high</option>'
+        + '</select>'
+        + '<button style="margin-left: 0.3rem" type="button" id="create_answer' + numberID + '" name="create" class="pure-button pure-button-primary" onClick="createAnswer(' + numberID + ',' + questionId + ')">New answer</button>'
+        + '<button style="margin-left: 0.3rem" type="button" id="add_answer_' + numberID + '" name="update" class="pure-button pure-button-primary" onClick="addAnswerExistingQuestion(this.id)"><i class="fa fa-plus"></i></button></div>');
+
+    $('#' + id).remove();
+    console.log("parent: " + parent);
+    console.log("add answer id: " + realID);
+}
+
+//
 // Get answers for a given question ID
 //
 $('#get_answers').on("click", function () {
@@ -129,6 +175,7 @@ $('#get_answers').on("click", function () {
             console.log(data[0].answer_id);
             var n = 1;
             var i;
+            var question_id = $("#question_title").val();
             $('#question_results').empty();
             $('#question_results').append('<form class="pure-form pure-form-aligned" id="form_answers">'
                                             + '<div class="pure-control-group">'
@@ -139,9 +186,9 @@ $('#get_answers').on("click", function () {
                                             + '</form>');
             
             for (i = 0; i < data.length; i++) {
-                 $('#form_answers').append('<div id="answer_results" class="pure-control-group">'
-                    + '<label for="answer_1">Answer # ' + n + '</label><input class="pure-input-1-3" id="'+ data[i].answer_id + '" type="text" value="' + data[i].answer_content + '">' 
-                    + '<select id="difficulty_answer_' + n + '" style="margin-left: 0.3rem"></select></div>');
+                 $('#form_answers').append('<div id="answer_results_' + n + '" class="pure-control-group">'
+                    + '<label for="answer_' + n + '">Answer # ' + n + '</label><input class="pure-input-1-3" id="'+ data[i].answer_id + '" type="text" value="' + data[i].answer_content + '">' 
+                    + '<select id="difficulty_answer_' + n + '" style="margin-left: 0.3rem"></select>');
 
                  if (data[i].answer_difficulty.toLowerCase() === 'low') {
                     $('#difficulty_answer_' + n).append('<option value="low" selected>low</option>'
@@ -159,31 +206,78 @@ $('#get_answers').on("click", function () {
                                                         + '<option value="high" selected>high</option>');
                  }
                 
-                $('#difficulty_answer_' + n).after('<button style="margin-left: 0.3rem" type="button" id="update_answer_1" name="update" class="pure-button pure-button-primary" onClick="updateAnswer(this.id, false)">Update</button>'
-                                            + '<button style="margin-left: 0.3rem" type="button" id="delete_answer_1" name="update" class="pure-button button-error" onClick="deleteAnswer(this.id, false)">Delete</button>');
+                $('#difficulty_answer_' + n).after('<button style="margin-left: 0.3rem" type="button" id="update_answer_' + n + '" name="update" class="pure-button pure-button-primary" onClick="updateAnswer(this.id, false)">Update</button>'
+                                            + '<button style="margin-left: 0.3rem" type="button" id="delete_answer_' + n + '" name="update" class="pure-button button-error" onClick="deleteAnswer(this.id, false)">Delete</button>');
                  n++;
             }
+            $('#delete_answer_' + (n - 1)).after('<button style="margin-left: 0.3rem" type="button" id="add_answer_' + (n - 1) + '" name="update" class="pure-button pure-button-primary" onClick="addAnswerExistingQuestion(this.id,' + question_id + ')"><i class="fa fa-plus"></i></button></div>');
            
         })
         .error(function () {
             //alert("error");
-        });
+        })
     return false;
 });
 
-$('#create_new_question').on("click", function () {
-    alert($('#form_new_question').serialize());
+//
+// Send data of the new question to the back end
+//
+$('#create_new_question').on("click", function() {
+    console.log("click");
+    var tmp = $('#form_new_question').serialize();
+    var split_obj = tmp.split('&');
+    var i;
+    var value;
+    var empty = false;
+    for (i = 0; i < split_obj.length; i++) {
+        value = split_obj[i].split('=');
+        if (value[1].trim().toLowerCase() === '') {
+            console.log("vacio");
+            empty = true;
+            break;
+        } else {
+            console.log("no vacio");
+        }
+    }
+    if (empty) {
+        $('#notification_messages').attr('class', 'pure-alert pure-alert-error');
+        $('#notification_messages').append('<strong>Missing fields</strong>. All fields must have a value before submitting.');
+        $('#notification_messages').show();
+    } else {
+        $('#notification_messages').hide();
+        console.log(JSON.stringify($('#form_new_question').serializeArray()));
+        $.ajax({
+            type: "POST",
+            url: '/completeQuestion',
+            dataType: 'json',
+            ContentType: 'application/json',
+            data: $('#form_new_question').serialize()
+        })
+            .success(function (data) {
+                alert("ok");
+                console.log(data);
+                //$('#answer_' + realID).parent().remove();
+            })
+            .error(function () {
+                alert("error");
+                
+            })
+        return false;
+    }
+    
 });
 
 $('#check_questions').on("click", function () {
     $('#questions').show();
     $('#new_question_result').hide();
+    $('#notification_messages').hide();
     
 });
 
 $('#new_question').on("click", function () {
     $('#new_question_result').show();
     $('#questions').hide();
+    $('#notification_messages').hide();
     $('#question_results').empty();
     
 });
